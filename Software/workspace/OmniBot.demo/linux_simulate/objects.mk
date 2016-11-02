20dbc53cc7dc4c75343e10561469313adc12483d
dwsindorf <dean.sindorf@gmail.com>
2016-11-01 21:25:26 -0600

    fixes for linux builds

--- Software/workspace/OmniBot.demo/linux_simulate/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lntcore -lwpilibcSim -lgz_msgs -lgazebo_client -lboost_system
-
